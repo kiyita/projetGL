92d79902cec14754adf07fa2d8fe9e1da920ce1a
dallatIkes <samy.chaabi0@gmail.com>
2025-02-21 16:46:27 +0100

    NPC speaks when near

--- Entities/NPC/Scripts/npc.gd
+++ Entities/NPC/Scripts/npc.gd
@@ -5,6 +5,7 @@ extends Entity
 @onready var dialogBox = $"3D_dialogBox"
 @onready var dialogBox_scene = dialogBox.get_scene_instance()
 
+var isSpeaking = false
 var defaultText = "Hello there! I am a basic Non Player Character (NPC), my duty is not determined yet but I am happy to speak with you :)"
 
 # Say if the npc is interacting with the player
@@ -19,18 +20,22 @@ func dialog() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
-	self.dialog()
-
+	#self.dialog()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	super._process(delta)
 	var player_position = get_parent_node_3d().get_parent_node_3d().get_node("Player").global_position
 	var dist_player = Movement.distanceVect(position, player_position)
-	if dist_player < 20:
-		pass
+	
+	if dist_player <= 5:
+		if not isSpeaking:
+			isSpeaking = true
+			dialogBox_scene.visible = true
+			self.dialog()  # Trigger the text animation only once
 	else:
-		pass
+		isSpeaking = false
+		dialogBox_scene.visible = false
+
 
 # Called when an area 3D enters this creature's area 3D
 func _on_area_3d_area_entered(area: Area3D) -> void:

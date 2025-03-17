6540fae68b8281451cfe828b85aadbfa34ef1ca6
Salazar2244 <157654504+Salazar2244@users.noreply.github.com>
2025-03-17 17:01:11 +0100

    Add menu sfx

--- UI/Scripts/menuButtons.gd
+++ UI/Scripts/menuButtons.gd
@@ -1,5 +1,6 @@
 extends Button
-
+@onready var sfx1 = $"sfx1"
+@onready var sfx2 = $"sfx2"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.reset_menu.connect(reset_menu)
@@ -10,18 +11,42 @@ func _process(delta):
 	pass
 
 func _on_reset_button_pressed():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	get_tree().paused = !get_tree().paused
 	get_tree().reload_current_scene()
 
 
 func _on_quit_button_pressed():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	get_tree().quit()
 
 
 func _on_parameters_button_pressed():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	$"..".visible = false;
 	$"../../VBoxContainer2".visible = true;
 
 func reset_menu():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	$"..".visible = true;
 	$"../../VBoxContainer2".visible = false;
--- UI/Scripts/parametersMenu.gd
+++ UI/Scripts/parametersMenu.gd
@@ -1,6 +1,8 @@
 extends Node
 
 signal new_angle_value(angle)
+@onready var sfx1 = $"sfx1"
+@onready var sfx2 = $"sfx2"
 
 func _ready():
 	$HSlider.value = Global.angle;
@@ -9,13 +11,31 @@ func _process(delta: float) -> void:
 	$Label2.text = $HSlider.value
 
 func _on_h_slider_drag_ended(value_changed):
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	Global.angle = $HSlider.value;
 
 func _on_return_button_pressed():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	$".".visible = false;
 	$"../VBoxContainer".visible = true;
 
 
 func _on_check_box_pressed():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
 	$HSlider.editable = !$HSlider.editable
 	Global.turnmode_changing()

43f414ab65f3a1ea316c9428e56a6bc661a6f8f1
EthanAudoux <ethan@audoux.net>
2025-04-04 16:53:02 +0200

    Add projectile for ennemi who have distance attack

--- Entities/Creature/Scripts/creature.gd
+++ Entities/Creature/Scripts/creature.gd
@@ -6,8 +6,8 @@ extends Entity
 @export var attackSpeed : float ## Number of seconds between two attacks
 var drop : Array[Object] ## Array containing the items dropped once dead
 
-
 @onready var time_last_deal_damage : float
+
 
 func _ready() -> void:
 	time_last_deal_damage = Time.get_ticks_msec() 
@@ -27,15 +27,19 @@ func attack() -> void:
 	
 	if Time.get_ticks_msec() - time_last_deal_damage > attackSpeed*1000:
 		if aMode == aggressiveMode.MELEE:
-			if disToPlayer <= melee_distance+0.5:
+			if disToPlayer <= melee_distance+0.2:
 				player_scene.damage_player(damage)
 				time_last_deal_damage = Time.get_ticks_msec()
 		elif aMode == aggressiveMode.DISTANCE:
-			if disToPlayer <= dist_distance:
-				# Generate a projectile scene which take the direction to the current player position
-				# Instanciate it with the damage to deal and when the projectile touch the player, deal damage
-				#TODO
-				pass
+			if disToPlayer <= dist_distance+0.2:
+				var node_entities = get_tree().current_scene.get_node("Entities")
+				var projectile_scene : Projectile = load("res://Items/Projectile/Scene/projectile.tscn").instantiate()
+				projectile_scene.initial_position = global_position
+				projectile_scene.destination = player_scene.global_position
+				projectile_scene.speed = 10
+				projectile_scene.damage = damage
+				node_entities.add_child(projectile_scene)
+				time_last_deal_damage = Time.get_ticks_msec()
 		else :
 			pass
 			

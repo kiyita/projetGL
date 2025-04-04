c913fdb98c5d394c1b96c036768db684bbc94d41
Salazar2244 <157654504+Salazar2244@users.noreply.github.com>
2025-04-04 14:52:00 +0200

    Adding sfx to the menu

--- UI/Scripts/parametersMenu.gd
+++ UI/Scripts/parametersMenu.gd
@@ -23,33 +23,18 @@ func _process(delta: float) -> void:
 	
 
 func _on_h_slider_drag_ended(value_changed):
-	randomize()  # Initialise the random number generator
-	var result = randi_range(1, 2)  #Generate 1 or 2
-	if(result == 1):
-		sfx1.play()
-	else : 
-		sfx2.play()
+	playRandomSfx()
 	Global.angle = $HSlider.value;
 	$Label2.text = str($HSlider.value)
 
 func _on_return_button_pressed():
-	randomize()  # Initialise the random number generator
-	var result = randi_range(1, 2)  #Generate 1 or 2
-	if(result == 1):
-		sfx1.play()
-	else : 
-		sfx2.play()
+	playRandomSfx
 	$".".visible = false;
 	$"../VBoxContainer".visible = true;
 
 
 func _on_check_box_pressed():
-	randomize()  # Initialise the random number generator
-	var result = randi_range(1, 2)  #Generate 1 or 2
-	if(result == 1):
-		sfx1.play()
-	else : 
-		sfx2.play()
+	playRandomSfx()
 	$HSlider.editable = !$HSlider.editable
 	Global.turnmode_changing()
 
@@ -57,6 +42,7 @@ func _on_check_box_pressed():
 
 # checkbox to change main hand
 func _on_main_hand_check_box_pressed() -> void:
+	playRandomSfx()
 	var player_scene = get_tree().current_scene.get_node("Player")
 	if player_scene.main_hand == PlayerScript.Hands.RIGHT:
 		player_scene.main_hand = PlayerScript.Hands.LEFT
@@ -65,6 +51,7 @@ func _on_main_hand_check_box_pressed() -> void:
 	change_text_checkbox_main_hand()
 
 func change_text_checkbox_main_hand():
+	playRandomSfx()
 	var player_scene = get_tree().current_scene.get_node("Player")
 	$MainHandCheckBox.text = "Main hand : "
 	if player_scene.main_hand == PlayerScript.Hands.LEFT:
@@ -76,5 +63,14 @@ func change_text_checkbox_main_hand():
 
 
 func _on_h_slider_player_speed_drag_ended(value_changed: bool) -> void:
+	playRandomSfx()
 	node_hand.max_speed = $HSliderPlayerSpeed.value
 	$LabelPlayerSpeed.text = node_hand.max_speed
+
+func playRandomSfx():
+	randomize()  # Initialise the random number generator
+	var result = randi_range(1, 2)  #Generate 1 or 2
+	if(result == 1):
+		sfx1.play()
+	else : 
+		sfx2.play()
